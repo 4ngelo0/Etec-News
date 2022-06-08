cc4f2d18ea9e112c01ab817be800fcc3b79d5a4b
4ngelo0 <gabrielb4rr00s@gmail.com>
2022-06-08 00:56:00 -0300

    home

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.menu-btn');
+const btn_close = document.querySelector('.fechar');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.querySelector('.sidebar');
+    nav.classList.toggle('active');
+    const btn = document.querySelector('.menu-btn');
+    btn.classList.toggle('active');
+
+}
+
+btn.addEventListener('click', toggleMenu);
+btn.addEventListener('touchstart', toggleMenu);
+btn_close.addEventListener('click', toggleMenu);
+btn_close.addEventListener('touchstart', toggleMenu);
